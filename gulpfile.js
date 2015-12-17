4aeb444096ec013c47dbc86209095732ad1e788d
Simon Lepel <sl@bytepark.de>
2015-12-17 16:45:00 +0100

    add build process and package.json

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var path = require('path');
+
+var csswring = require('csswring'),
+    extReplace = require('gulp-ext-replace'),
+    gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    plumber = require('gulp-plumber'),
+    stylus = require('gulp-stylus');
+
+var paths = {
+  cwd: __dirname,
+  stylus: path.join(__dirname, 'stylus'),
+  css: path.join(__dirname, 'css')
+};
+
+gulp.task('build:css', function() {
+  return gulp
+    .src(path.join(paths.stylus, '*.styl'))
+    .pipe(plumber())
+    .pipe(stylus({
+      paths: [
+        path.join(paths.stylus, 'imports')
+      ]
+    }))
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('build:css-minified', ['build:css'], function() {
+  return gulp
+    .src([
+      path.join(paths.css, '*.css'),
+      '!' + path.join(paths.css, '*.min.css')
+    ])
+    .pipe(plumber())
+    .pipe(postcss([
+      csswring({
+        preserveHacks: true
+      })
+    ]))
+    .pipe(extReplace('.min.css'))
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('default', ['build:css-minified']);
